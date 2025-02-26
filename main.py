f52101961259c1082231b921cebce32c0a646b27
fatnaoui <hamzafatnaoui.ai@gmail.com>
2025-02-26 18:00:51 +0000

    function to convert pdf to text using pdfplumber

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from tools import extract_text_from_pdf
+from tools import extract_text_from_pdf, extract_text_from_pdf_and_create_file
+
+pdf_path = "Hamza_Fatnaoui_CV.pdf"
+text_dir = "converted_text"
+name_condidat = "Hamza_Fatnaoui"
+
+extract_text_from_pdf_and_create_file(pdf_path,text_dir,name_condidat)
--- tools/simple/Document_Handling.py
+++ tools/simple/Document_Handling.py
@@ -9,6 +9,17 @@ def extract_text_from_pdf(pdf_path):
     except Exception as e:
         print(e)
 
+# pdf to text converter and create file text
+def extract_text_from_pdf_and_create_file(pdf_path,text_dir,name_condidat):
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
+    except Exception as e:
+        print(e)
+
+    with open(f"{text_dir}/{name_condidat}.txt",'w') as f:
+        f.write(text)
+
 if __name__=='__main__':
     print("This is a python script that convert pdf documents into text")
 
