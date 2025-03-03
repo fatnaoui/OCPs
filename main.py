ffc10d590b982f6a7b4819c685457d838fc4c929
fatnaoui <hamzafatnaoui.ai.com>
2025-03-03 11:17:53 +0000

    playing around

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-from tools import (extract_text_from_pdf, 
+from tools import ( extract_text_from_pdf, 
                     extract_text_from_pdf_and_create_file,
-                    )
+                    doc_to_image )
 
-import torch
-from PIL import Image
-from transformers import AutoModel, AutoTokenizer
+input_dir = "./data" # where out data is stored
+output_dir = "./converted_data" # where we are gonna store our data
 
+# pdf_path = "Hamza_Fatnaoui_CV.pdf"
+# text_dir = "converted_text"
+# name_condidat = "Hamza_Fatnaoui"
 
-pdf_path = "Hamza_Fatnaoui_CV.pdf"
-text_dir = "converted_text"
-name_condidat = "Hamza_Fatnaoui"
-
-# I worked with Gemma "google/gemma-2-2b-it"
-cache_dir = "models/MiniCPM_o_2_6"
-
-model = AutoModel.from_pretrained(
-    'openbmb/MiniCPM-o-2_6',
-    trust_remote_code=True,
-    attn_implementation='sdpa', # sdpa or flash_attention_2
-    torch_dtype=torch.bfloat16,
-    init_vision=True,
-    init_audio=False,
-    init_tts=False,
-    cache_dir=cache_dir
-)
-
-model = model.eval().cuda()
-tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-o-2_6', trust_remote_code=True)
+doc_to_image(input_dir,output_dir)
 
 
 
--- tools/simple/doc_to_image.py
+++ tools/simple/doc_to_image.py
@@ -4,21 +4,24 @@ import os
 def doc_to_image(input_dir,output_dir):
 
     if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+        os.makedirs(output_dir)
        
     for filename in os.listdir(input_dir):
         if filename.endswith(".pdf") or filename.endswith(".docx"):
+            condidatname = os.path.splitext(filename)[0]
             file_input_path = os.path.join(input_dir,filename)
-            output_path = os.path.join(output_dir,f"{os.path.splitext(filename)[0]}")
+            output_path = os.path.join(output_dir,condidatname)
+            os.makedirs(output_path)
         else:
             raise Exception("Extension doesn't match a 'pdf' or a 'docx'")
 
         try: 
             images = convert_from_path(file_input_path)
             for i, img in enumerate(images):
-                img_path = f"{os.path.join(output_path,f"{os.path.splitext(filename)[0]}")}_{i}.jpg"
+                img_path = os.path.join(output_path,f"{condidatname}_{i}.jpg")
                 img.save(img_path, 'JPEG')
+            print(f"✅ Converted {filename} to images successfully!")
         except Exception as e:
-            print(f'Error converting {pdf_path}: {e}')
+            print(f"❌ Error converting {filename}: {e}")
 
 
