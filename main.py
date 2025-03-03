97c062ba10872c6484c119e733767df2a79f9b96
fatnaoui <hamzafatnaoui.ai.com>
2025-03-03 11:22:38 +0000

    playing around

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from tools import ( extract_text_from_pdf,
 input_dir = "./data" # where out data is stored
 output_dir = "./converted_data" # where we are gonna store our data
 
-# pdf_path = "Hamza_Fatnaoui_CV.pdf"
-# text_dir = "converted_text"
-# name_condidat = "Hamza_Fatnaoui"
-
 doc_to_image(input_dir,output_dir)
 
 
