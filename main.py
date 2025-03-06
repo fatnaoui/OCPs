8c4f4bdd01819dbbef44901cb83bd0d53d815e05
fatnaoui <hamzafatnaoui.ai.com>
2025-03-06 12:39:50 +0000

    prompt interating

--- main.py
+++ main.py
@@ -14,29 +14,18 @@ image_dir = "./data/image_data" # where we are gonna store our images data
 output_dir = "./data/output_data" # where output data from the model is stored
 
 # Handling the Prompts
-resume_extract = "./prompts/resume_extraction_prompt.txt"
+resume_extraction_prompt = "./prompts/resume_extraction_prompt.txt"
 
 # Converting all Resumes to Images and Store them 
 # doc_to_image(input_dir,output_dir)
 
 # Load the Model from the Cache
 omni_model , omni_tokenizer = download_omni()
-# smolvl_model, smolvl_processor = download_smolvl()
 
-image = "./data/image_data/Hamza_Fatnaoui_CV/Hamza_Fatnaoui_CV_0.jpg"
-images = ["./data/image_data/Hamza_Fatnaoui_CV/Hamza_Fatnaoui_CV_0 copy.jpg",
-          "./data/image_data/Hamza_Fatnaoui_CV/Hamza_Fatnaoui_CV_0.jpg"]
-# image1 = "./converted_data/Academic___Taha_Bouhsine/Academic___Taha_Bouhsine_0.jpg"
-# # image2 = "./converted_data/Academic___Taha_Bouhsine/Academic___Taha_Bouhsine_1.jpg"
-
-with open(resume_extract,'r') as f:
+with open(resume_extraction_prompt,'r') as f:
   prompt = f.read()
 
 print("It starts")
-
-# start = time.time()
-# res = generate_from_omni(omni_model,omni_tokenizer,prompt,image)
-# end = time.time()
 
 start = time.time()
 res = generate_from_omni_for_multiple_image(omni_model,omni_tokenizer,prompt,images)
@@ -44,13 +33,15 @@ end = time.time()
 
 latency = end - start
 print(f"Latency time is: {latency}")   # Latency time is: 145.90626096725464
-# # res = generate_from_smolvl(smolvl_model,smolvl_processor,prompt,image)
+# res = generate_from_smolvl(smolvl_model,smolvl_processor,prompt,image)
 
 with open('twoImage.json','w') as f:
     f.write(res)
 
 print("You Done Here")
 
+
+  
 
 
 
---
+++ tools/simple/resume_extraction.py
@@ -0,0 +1,47 @@
+from tools import generate_from_omni_for_multiple_image
+
+import os
+from tqdm import tqdm
+import time
+
+
+
+def resume_extraction(image_data,output_data,model,tokenizer,prompt_file):
+    '''Here, we are assuming that data 'resumes' is already converted to images,
+    And we're gonna extract all the infromation in condidate's resume, and
+    store it as json file to work with it later'''
+
+    if not os.path.isdir(image_data):
+        raise ValueError(f"Invalid directory path: {image_data}")
+
+    with open(prompt_file,'r') as f:
+        prompt = f.read()
+
+    candidates = [c for c in os.listdir(image_data) if not c.startwith(".")]
+
+    for candidate in tqdm(candidates, desc="Processing Resumes", unit="Resume"):
+        candidate_path = os.path.join(image_data,candidate)
+
+        # Validate candidate path
+        if not os.path.isdir(candidate_path):
+            continue
+
+        # Get all resume pages (filter hidden files)
+        candidate_cv_pages = [
+            os.path.join(candidate_path, page)
+            for page in os.listdir(candidate_path)
+            if not page.startswith(".")
+        ]
+        condidate_output_path = os.path.join(output_data,f"{candidate}.json")
+
+        try:
+            res = generate_from_omni_for_multiple_image(model,tokenizer,prompt,candidate_cv_pages)
+
+            with open(condidate_output_path,'w') as f:
+                f.write(res)
+        except Exception as e:
+            print("Error processing {condidate}: {e}")
+            continue
+    
+    print("You extracted data is ready to analyse")
+
